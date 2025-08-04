231b1564e011813a802c4db6e2e346a22eccd513
ailtonjunior11 <ailtonjunior.dev@gmail.com>
2025-08-04 19:50:41 -0300

    evento de click adicionado ao background, para ativar e desativar os icones abrir e fechar do menu.

--- scripts.js
+++ scripts.js
@@ -33,6 +33,10 @@ background.addEventListener("click", () => {
   menuButton.classList.remove("active");
   background.classList.remove("active");
 
+  document.body.style.backgroundColor = "#c6d9e7";
+}); // Aqui se adiciona o evento de clique no background, que remove a classe "active" do menu lateral, do conteúdo principal e do botão do menu, e também muda a cor de fundo do body.
+
+background.addEventListener("click", () => {
   if (menuButtonOpen.style.delay === "inline") {
     menuButtonOpen.style.display = "none";
     menuButtonClose.style.display = "inline";
@@ -40,9 +44,7 @@ background.addEventListener("click", () => {
     menuButtonOpen.style.display = "inline";
     menuButtonClose.style.display = "none";
   } // Nesse evento, quando o usuário clicar na área de fundo, o menu lateral, o conteúdo principal e o botão do menu são fechados.
-
-  document.body.style.backgroundColor = "#c6d9e7";
-}); // Aqui se adiciona o evento de clique no background, que remove a classe "active" do menu lateral, do conteúdo principal e do botão do menu, e também muda a cor de fundo do body.
+});
 
 // Swiper sendo inicializado
 const swiper = new Swiper(".swiper", {
